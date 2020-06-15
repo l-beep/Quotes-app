6436d99441e59e18a9bf2f8dfe45a1f3f7e4879a
LOvine <oduor.lovine@gmail.com>
2020-06-15 13:19:15 +0300

    generated count-input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { CountInputComponent } from './count-input/count-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    CountInputComponent
   ],
   imports: [
     BrowserModule,
