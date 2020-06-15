d23100c4aa4512a210899f49388d192aa81bc684
LOvine <oduor.lovine@gmail.com>
2020-06-15 18:31:03 +0300

    add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Quote } from './quote'
 })
 export class AppComponent {
   quotes:Quote[] = [
-    new Quote(1, 'Dont look at the clock, do what it does keep moving', 'Biron Lovine', new Date(2020,4,21)),
-    new Quote(2, 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'Rihannah', new Date(2019,5,16)),
-    new Quote(3, 'Success is a battle located in the brain only the fittest can survive', 'Lovine Biron', new Date(2020,1,21)),
+    new Quote(1, 'No one cares about your struggles and failures but everyone carse about your success and achievements', 'Lovine Oduor', new Date(2020,6,15)),
+    new Quote(2, 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'Rihannah', new Date(2020,6,15)),
+    new Quote(3, 'Success is a battle located in the brain that only the relentless can survive', 'Lovine Oduor', new Date(2020,6,15)),
   ];
 }
