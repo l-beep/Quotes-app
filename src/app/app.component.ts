ad5a9af2abeba367ca0ae73e00f8cc7f0da054a3
LOvine <oduor.lovine@gmail.com>
2020-06-15 14:16:27 +0300

    add quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quote';
+  quotes:Quote[] = [
+    new Quote(1, 'Dont look at the clock, do what it does keep moving', 'Biron Lovine', new Date(2020,4,21)),
+    new Quote(2, 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'Rihannah', new Date(2019,5,16)),
+    new Quote(3, 'Success is a battle located in the brain only the fittest can survive', 'Lovine Biron', new Date(2020,1,21)),
+  ];
 }
