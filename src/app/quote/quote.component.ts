c74e43523be0c222e01bf827504c88d06d50d363
LOvine <oduor.lovine@gmail.com>
2020-06-15 14:32:31 +0300

    add newQuote function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+
+  quotes: Quote[] = [
+    new Quote(1, 'Dont look at the clock, do what it does keep moving', 'Biron Lovine', new Date(2020,4,21)),
+    new Quote(2, 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'LucycWigwa', new Date(2019,5,16)),
+    new Quote(3, 'Success is a battle located in the mind and only the fittest can survive', 'Lovine Biron', new Date(2020,1,21)),
+  ];
+
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+
+
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  addVote(quote, index) {
+    if (quote) {
+      this.quotes[index].upvote += 1;
+    }
+  }
+  lessVote(quote, index) {
+    if (quote) {
+      this.quotes[index].downvote += 1
+    }
+  }
+  initNum: number
+  finNum: number
+  counter: number
+
+  mostLiked() {
+    this.initNum = 0
+    this.finNum = 0
+    for (this.counter = 0; this.counter < this.quotes.length; this.counter++) {
+      this.finNum = this.quotes[this.counter].upvote;
+
+      if (this.finNum > this.initNum) {
+        this.initNum = this.finNum
+      }
+
+
+
+    }
+
+    return this.initNum;
+  }
 
   constructor() { }
 
