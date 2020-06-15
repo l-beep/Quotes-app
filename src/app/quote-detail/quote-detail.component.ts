088b9dedf7fec41e3fc7167bb36d4a5b5bad7cf9
LOvine <oduor.lovine@gmail.com>
2020-06-15 13:59:53 +0300

    add voting

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from "../quote"
 
 @Component({
   selector: 'app-quote-detail',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailComponent implements OnInit {
 
+  @Input() quote :Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  @Output () voteIncrease = new EventEmitter<boolean>();
+  @Output () voteDecrease = new EventEmitter<boolean> ();
+   ifIncrease(one:boolean){
+     this.voteIncrease.emit(one);
+
+   }
+   ifDecrease(two:boolean) {
+     this.voteDecrease.emit(two);
+   }
+quoteDan(purge:boolean){
+  this.isComplete.emit(purge);
+
+}
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
